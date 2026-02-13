881f651fcea26f76995348cf0d7847a6c6b048e8
arya pramudya <aryapramudya610@gmail.com>
2026-02-13 12:22:55 +0700

    Buat Game Screen

---
+++ lib/game/game_screen.dart
@@ -0,0 +1,55 @@
+
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            top: 50,
+            left: 20,
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(10),
+              ),
+
+              child: const Text(
+                'Score: 1',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 50,
+            right: 20,
+            child: Row(
+              children: [
+                IconButton(
+                 icon: const Icon(Icons.music_note),
+                 onPressed: () {
+                   
+                 },
+                ),
+                
+              ],
+            ),)
+        ],
+      ),
+    );
+  }
+}
