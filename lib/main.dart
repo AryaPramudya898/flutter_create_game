fcf33231747eecb4156f780db3196f11f1990f3c
arya pramudya <aryapramudya610@gmail.com>
2026-02-13 21:10:31 +0700

    Memperbaiki Main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:game/game/game_screen.dart';
-
+import 'package:game/game/fruit_catcher_game.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,11 +8,83 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Fruit Catcher Game',
-     home:GameScreen(),
-     debugShowCheckedModeBanner: false,
-     );
+    return MaterialApp(
+      title: 'Flutter Game',
+      home: GameScreen(),
+    );
+  }
+}
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  // final ValueNotifier<int> counter = ValueNotifier(0);
+  late FruitCatcherGame game;
+
+  @override
+  void initState() {
+    super.initState();
+    game = FruitCatcherGame();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(   
+      body: Stack(
+        children: [
+          GameWidget(game: game),
+          Positioned(
+            top: 50,
+            left: 20,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius : BorderRadius.circular(10)
+              ),
+              child: ValueListenableBuilder<int>(
+                valueListenable: game.scoreNotifier,
+                builder: (context, score, child) {
+                  return Text(
+                    'Score: $score',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold
+                    ),
+                  );
+                },
+              )
+            ),
+          ),
+          Positioned(
+            top: 50,
+            right: 20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.music_note),
+                  onPressed: () {}, 
+                ),
+                IconButton(
+                  icon: Icon(Icons.volume_up),
+                  onPressed: () {}, 
+                ),
+              ],
+            )
+          ),
+        ],
+      ),
+    );
   }
 }
