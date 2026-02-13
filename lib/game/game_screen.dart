1f50705ee4fd1aa18991a1cc0a74e6517117cf0b
arya pramudya <aryapramudya610@gmail.com>
2026-02-13 13:13:43 +0700

    buat padding untuk tambah score

--- lib/game/game_screen.dart
+++ lib/game/game_screen.dart
@@ -1,16 +1,14 @@
-
 import 'package:flutter/material.dart';
 
 class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
 
   @override
-  State<GameScreen> createState() => _GameScreenState();
+  State<GameScreen> createState() => _GamescreenState();
 }
 
-class _GameScreenState extends State<GameScreen> {
-  final ValueNotifier<int> counter = ValueNotifier(1);
-
+class _GamescreenState extends State<GameScreen> {
+  final ValueNotifier<int> Counter = ValueNotifier(1);
 
   @override
   void initState() {
@@ -20,53 +18,71 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Expanded(
-        child: Stack(
-          children: [
-            Positioned(
-              top: 50,
-              left: 20,
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: ValueListenableBuilder<int>(
-                  valueListenable: counter,
-                  builder: (context, score, child) {
-                    return Text(
-                      'Score: $score',
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 50,
+                  left: 20,
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.7),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ValueListenableBuilder<int>(
+                      valueListenable: Counter,
+                      builder: (context, score, child) {
+                        return Text(
+                          'Score: $score',
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ), 
+            
+                Positioned(
+                  top: 50,
+                  right: 20,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.music_note, color: Colors.black),
+                        onPressed: () {
+                          // Handle volume up action
+                        },
                       ),
-                    );
-                  },
+                      IconButton(
+                        icon: const Icon(Icons.volume_up),
+                        onPressed: () {
+                          // Handle volume off action
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              ],
             ),
-
-            Positioned(
-              top: 50,
-              right: 20,
-              child: Row(
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.music_note),
-                    onPressed: () {},
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.volume_up),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ), // 
+          ),
+          Padding(
+    padding: const EdgeInsets.all(16),
+    child: ElevatedButton(
+      onPressed: () {
+        Counter.value++;
+      },
+      child: const Text("Tambah Score"),
+    ),
+ ),
+ // posisi paling bawah pada widget array column 
+        ],
+      ),
     );
   }
 }
