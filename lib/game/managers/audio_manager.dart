025ac4ff42e11b2e7364e0390a6aa8c3ee4f2d3f
AryaPramudya898 <aryapramudya610@gmail.com>
2026-02-17 16:31:16 +0700

    tambah audio

--- lib/game/managers/audio_manager.dart
+++ lib/game/managers/audio_manager.dart
@@ -1,3 +1,29 @@
+import 'package:flame_audio/flame_audio.dart';
+
 class AudioManager {
-  
+  static final AudioManager _instance = AudioManager._internal();
+  factory AudioManager() => _instance;
+  AudioManager._internal();
+
+  bool _isMusicEnabled = true;
+  bool _isSfxEnabled = true;
+  double _musicVolume = 0.7;
+  double _sfxVolume = 1.0;
+
+  bool get isMusicEnabled => _isMusicEnabled;
+  bool get isSfxEnabled => _isSfxEnabled;
+  double get musicVolume => _musicVolume;
+  double get sfxVolume => _sfxVolume;
+
+  Future<void> initialize() async {
+    try {
+      await FlameAudio.audioCache.loadAll([
+        'music/arcade_melody.mp3',
+        'sfx/wolfy_sanic-collect-ring-15982.mp3',
+      ]);
+      print('Audio Initialize Successfully');
+    } catch (e) {
+        print('Error initializing audio: $e');
+      }
+  }
 }
