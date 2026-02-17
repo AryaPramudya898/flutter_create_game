2745337e4eed0021789cc603a02a798f4843d25c
AryaPramudya898 <aryapramudya610@gmail.com>
2026-02-17 16:39:14 +0700

    ubah main ke aynchronos dan panggil init audio manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:game/game/fruit_catcher_game.dart';
+import 'package:game/game/managers/audio_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AudioManager().initialize();
+
   runApp(const MyApp());
 }
 
